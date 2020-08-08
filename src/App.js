407f9aa908e2f4090f4823d3b304d302ba8480f1
kk77777 <kaushikgattani@gmail.com>
2020-08-08 22:22:17 +0530

    final touchup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Fragment } from 'react';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 import './App.css';
 
 const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <Fragment>
       <LoginButton />
